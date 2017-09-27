d02247cf71c324065d12d9963663dfd52f25ad2c
Candy <Nooolie@gmail.com>
2017-09-27 23:06:42 +0300

    some image changed

--- src/reducers/reducer-products.js
+++ src/reducers/reducer-products.js
@@ -2,23 +2,23 @@ export default function(){
   return [
     {
       id: 1,
-      name: 'red hat',
-      image: 'http://www.villagehatshop.com/photos/product/standard/4511390S61354/all/pachuco-crushable-wool-felt-fedora-hat.jpg',
-      price: 50,
+      name: 'figure vivi',
+      image: './image/figure-vivi.jpg',
+      price: 34,
       quantity: 1
     },
     {
       id: 2,
-      name: 'white hat',
-      image: 'http://www.villagehatshop.com/photos/product/standard/4511390S171094/all/panama-straw-c-crown-fedora-hat.jpg',
-      price: 30,
+      name: 'figure choco',
+      image: './image/figure-chocobo.jpg',
+      price: 25,
       quantity: 1
     },
     {
       id: 3,
-      name: 'yellow hat',
-      image: 'http://www.villagehatshop.com/photos/product/standard/4511390S412353/all/black-band-wheat-straw-skimmer-hat.jpg',
-      price: 20,
+      name: 'figure cloud',
+      image: './image/figure-cloud.jpg',
+      price: 55,
       quantity: 1
     }
   ]
